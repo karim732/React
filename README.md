5856a97a1cee76165c7e70ed855fa855e770297a
Shaik Karimulla <karimulla.s@prodapt.com>
2023-10-05 15:24:37 +0530

    state management

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-"# React" 
+"# React"
+Drawback of ContextAPI
+
+- Every component that use useContext will re build, no matter whether it is required or not.
+
+- useEffect usecase - do something when comp mounts and umounts.
+  useEffect(() => {
+  listners.push(setState);
+
+  return () => {
+  listners = listners.filter((li) => li !== setState);
+  };
+  }, [setState]);
+
+- state management
+
+  1.  Redux(complex)
+  2.  contextAPI(simple)
+  3.  custom hook store(complex)(mix of useState, useEffect,..)
+
+- replace-redux-06-bonus-multiple-slices - where more than one state slice gets managed.
+  You can also use it to manage multiple, independent state slices though.
---
+++ replace-redux-01-starting-project/src/hooks-store/store.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+let globalState = {};
+
+let listners = [];
+
+let actions = {};
+
+export const useStore = (shouldListen = true) => {
+  const setState = useState(globalState)[1];
+
+  const dispatch = (actionIdentifier, payload) => {
+    const newState = actions[actionIdentifier](globalState, payload);
+    globalState = { ...globalState, ...newState };
+
+    for (const listner of listners) {
+      listner(globalState);
+    }
+  };
+
+  useEffect(() => {
+    if (shouldListen) {
+      listners.push(setState);
+    }
+
+    return () => {
+      if (shouldListen) {
+        listners = listners.filter((li) => li !== setState);
+      }
+    };
+  }, [setState, shouldListen]);
+
+  return [globalState, dispatch];
+};
+
+export const initStore = (userActions, initialState) => {
+  if (initialState) {
+    globalState = { ...globalState, ...initialState };
+  }
+  actions = { ...actions, ...userActions };
+};
